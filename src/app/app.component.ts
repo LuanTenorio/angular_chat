20b28c6b54208e2c0ce5a046304b84fd34a3f90d
LuanTenorio <lluantenorio7@gmail.com>
2022-04-22 17:26:02 -0300

    Style Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ChatService } from './chat/chat.service';
 
 @Component({
@@ -6,13 +6,22 @@ import { ChatService } from './chat/chat.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
 
   date = new Date().getTime()
-  constructor(
-    private chatService: ChatService
-    ){
-    this.chatService.getAllMessegesForUser()
+  constructor(private chatService: ChatService){
+    this.chatService.getAllMessegesForUser();
+    
+  }
+
+  ngOnInit(): void {
+    const headerHeight = document.querySelector('app-header')!.getBoundingClientRect().height;
+    const contentMessageHeight = document.querySelector('div.content-message')!.getBoundingClientRect().height;
+    const bodyHeight = document.querySelector('body')!.getBoundingClientRect().height;
+    const searchFriendHeight = document.querySelector('div.search-friend')!.getBoundingClientRect().height;
+    console.log(headerHeight)
+    document.body.style.setProperty('--chat-height', `${bodyHeight - (contentMessageHeight + headerHeight)}px`);
+    document.body.style.setProperty('--list-friends-height', `${bodyHeight - (headerHeight + searchFriendHeight)}px`); 
   }
 
 }
